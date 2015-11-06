849270b22aa4430ecff8603e5a28b01fbde2b647
Eduardo Aros <e.arosb@gmail.com>
2015-11-06 16:14:05 -0300

    GridView cambiado a RecyclerView y CardView

--- app/src/main/java/com/cl/earosb/iipzs/NuevoCEActivity.java
+++ app/src/main/java/com/cl/earosb/iipzs/NuevoCEActivity.java
@@ -2,6 +2,8 @@ package com.cl.earosb.iipzs;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
@@ -16,27 +18,29 @@ import com.cl.earosb.iipzs.model.NuevoCEAdapter;
  */
 public class NuevoCEActivity extends AppCompatActivity {
 
+    RecyclerView mRecyclerView;
+    RecyclerView.LayoutManager mLayoutManager;
+    RecyclerView.Adapter mAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nuevo_ce);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        GridView gridview = (GridView) findViewById(R.id.gridview);
-        gridview.setAdapter(new NuevoCEAdapter(this));
+        mRecyclerView = (RecyclerView)findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
 
-        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(NuevoCEActivity.this, "" + position, Toast.LENGTH_SHORT).show();
-            }
-        });
+        // The number of Columns
+        mLayoutManager = new GridLayoutManager(this, 2);
+        mRecyclerView.setLayoutManager(mLayoutManager);
 
-    }
+        mAdapter = new NuevoCEAdapter();
+        mRecyclerView.setAdapter(mAdapter);
 
-    public void holaPlusDiez(View v){
-        Log.d("TAG_plus", "|+10| " + v.getId());
     }
 
 }
--- app/src/main/java/com/cl/earosb/iipzs/model/NuevoCEAdapter.java
+++ app/src/main/java/com/cl/earosb/iipzs/model/NuevoCEAdapter.java
@@ -1,19 +1,14 @@
 package com.cl.earosb.iipzs.model;
 
 import android.content.Context;
-import android.support.design.widget.Snackbar;
-import android.util.Log;
+import android.os.Vibrator;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
 import android.widget.Button;
-import android.widget.GridView;
-import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.cl.earosb.iipzs.NuevoCEActivity;
 import com.cl.earosb.iipzs.R;
 
 import java.util.List;
@@ -21,62 +16,86 @@ import java.util.List;
 /**
  * Created by earosb on 22-10-15.
  */
-public class NuevoCEAdapter extends BaseAdapter {
-    private Context mContext;
+public class NuevoCEAdapter extends RecyclerView.Adapter<NuevoCEAdapter.ViewHolder> {
 
-    private List<Partida> partidas;
+    List<Partida> mPartidas;
 
-    private int cont = 0;
-
-    public NuevoCEAdapter(Context c) {
-        mContext = c;
-        partidas = Partida.getAll();
+    public NuevoCEAdapter() {
+        super();
+        mPartidas = Partida.getAll();
     }
 
-    public int getCount() {
-        return partidas.size();
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext())
+                .inflate(R.layout.grid_nuevo_ce, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder(v);
+        return viewHolder;
     }
 
-    public Object getItem(int position) {
-        return null;
+    @Override
+    public void onBindViewHolder(final ViewHolder viewHolder, int i) {
+        final Partida p = mPartidas.get(i);
+        viewHolder.name.setText(p.nombre);
+        viewHolder.cont.setText(String.valueOf(p.cont));
+        viewHolder.plus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int aux = Integer.parseInt(viewHolder.cont.getText().toString()) + 1;
+                viewHolder.cont.setText(String.valueOf(aux));
+            }
+        });
+        viewHolder.plus1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                int aux = Integer.parseInt(viewHolder.cont.getText().toString()) - 1;
+                viewHolder.cont.setText(String.valueOf(aux));
+                Vibrator v = (Vibrator) view.getContext().getSystemService(Context.VIBRATOR_SERVICE);
+                v.vibrate(500);
+                return true;
+            }
+        });
+        viewHolder.plus10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int aux = Integer.parseInt(viewHolder.cont.getText().toString()) + 10;
+                viewHolder.cont.setText(String.valueOf(aux));
+            }
+        });
+        viewHolder.plus10.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                int aux = Integer.parseInt(viewHolder.cont.getText().toString()) - 10;
+                viewHolder.cont.setText(String.valueOf(aux));
+                Vibrator v = (Vibrator) view.getContext().getSystemService(Context.VIBRATOR_SERVICE);
+                // Vibrate for 500 milliseconds
+                v.vibrate(500);
+                return true;
+            }
+        });
+
     }
 
-    public long getItemId(int position) {
-        return 0;
+    @Override
+    public int getItemCount() {
+
+        return mPartidas.size();
     }
 
-    public View getView(final int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    protected class ViewHolder extends RecyclerView.ViewHolder {
 
-        View gridView;
+        public TextView cont;
+        public TextView name;
+        public Button plus1;
+        public Button plus10;
 
-        if (convertView == null) {
-            gridView = inflater.inflate(R.layout.grid_nuevo_ce, null);
-
-            final Partida partida = partidas.get(position);
-
-            TextView txtPartida = (TextView) gridView.findViewById(R.id.txtview_partida_realizar);
-            txtPartida.setText(partida.nombre);
-
-            final TextView txtContador = (TextView) gridView.findViewById(R.id.txtview_contador);
-            txtContador.setText(0);
-
-            Button btn_plus_1 = (Button) gridView.findViewById(R.id.btn_plus_1);
-            btn_plus_1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Log.d("TAG_plus1", "|+1| " + view.getId());
-                    int aux = Integer.parseInt((String) txtContador.getText());
-                    txtContador.setText((aux + 1));
-                    notifyDataSetChanged();
-                }
-            });
-
-        } else {
-            gridView = (View) convertView;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            cont = (TextView) itemView.findViewById(R.id.card_cont);
+            name = (TextView) itemView.findViewById(R.id.card_text);
+            plus1 = (Button) itemView.findViewById(R.id.card_plus1);
+            plus10 = (Button) itemView.findViewById(R.id.card_plus10);
         }
-
-        return gridView;
     }
 
 }
