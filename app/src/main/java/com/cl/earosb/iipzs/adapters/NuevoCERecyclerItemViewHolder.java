a10ec0f4aca31fcbba0ea54b8af5c43501aede6f
Eduardo Aros <e.arosb@gmail.com>
2016-01-03 12:45:58 -0300

    Toolbar obs ready

--- app/src/main/java/com/cl/earosb/iipzs/adapters/NuevoCERecyclerItemViewHolder.java
+++ app/src/main/java/com/cl/earosb/iipzs/adapters/NuevoCERecyclerItemViewHolder.java
@@ -26,16 +26,14 @@ public class NuevoCERecyclerItemViewHolder extends RecyclerView.ViewHolder {
     private TextView mCardObs;
     private Button mPlus1;
     private Button mPlus10;
-    private Button mObs;
 
-    public NuevoCERecyclerItemViewHolder(final View parent, TextView partidaNombre, TextView trabajoCont, TextView trabajoObs, Button plus1, Button plus10, Button obs) {
+    public NuevoCERecyclerItemViewHolder(final View parent, TextView partidaNombre, TextView trabajoCont, TextView trabajoObs, Button plus1, Button plus10) {
         super(parent);
         mCardText = partidaNombre;
         mCardCont = trabajoCont;
         mCardObs = trabajoObs;
         mPlus1 = plus1;
         mPlus10 = plus10;
-        mObs = obs;
     }
 
     public static NuevoCERecyclerItemViewHolder newInstance(final View parent) {
@@ -43,11 +41,62 @@ public class NuevoCERecyclerItemViewHolder extends RecyclerView.ViewHolder {
         final TextView partidaCont = (TextView) parent.findViewById(R.id.card_cont);
         final TextView trabajoObs = (TextView) parent.findViewById(R.id.card_obs_text);
         trabajoObs.setMovementMethod(new ScrollingMovementMethod());
-        Button plus1 = null;//(Button) parent.findViewById(R.id.card_plus1);
-        Button plus10 = null;//(Button) parent.findViewById(R.id.card_plus10);
+        Button plus1 = (Button) parent.findViewById(R.id.card_plus1);
+        Button plus10 = (Button) parent.findViewById(R.id.card_plus10);
         Button obs = null;//(Button) parent.findViewById(R.id.card_obs);
-
         Toolbar toolbar = (Toolbar) parent.findViewById(R.id.card_toolbar);
+
+        plus1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Trabajo t = Trabajo.load(Trabajo.class, Long.parseLong(partidaName.getTag().toString()));
+                t.cantidad = t.cantidad + 1;
+                t.partida.ranking = t.partida.ranking + 1;
+                t.partida.save();
+                t.save();
+                partidaCont.setText(String.valueOf(t.cantidad));
+            }
+        });
+        plus1.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Trabajo t = Trabajo.load(Trabajo.class, Long.parseLong(partidaName.getTag().toString()));
+                if (t.cantidad > 0) {
+                    t.cantidad = t.cantidad - 1;
+                    t.save();
+                    partidaCont.setText(String.valueOf(t.cantidad));
+                }
+                return true;
+            }
+        });
+        plus10.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Trabajo t = Trabajo.load(Trabajo.class, Long.parseLong(partidaName.getTag().toString()));
+                t.cantidad = t.cantidad + 10;
+                t.partida.ranking = t.partida.ranking + 1;
+                t.partida.save();
+                t.save();
+                partidaCont.setText(String.valueOf(t.cantidad));
+            }
+        });
+        plus10.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Trabajo t = Trabajo.load(Trabajo.class, Long.parseLong(partidaName.getTag().toString()));
+                if (t.cantidad > 9) {
+                    t.cantidad = t.cantidad - 10;
+                    t.save();
+                    partidaCont.setText(String.valueOf(t.cantidad));
+                } else {
+                    t.cantidad = 0;
+                    t.save();
+                    partidaCont.setText(String.valueOf(t.cantidad));
+                }
+                return true;
+            }
+        });
+
         if (toolbar != null) {
             toolbar.inflateMenu(R.menu.card_nuevo_ce);
             toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
@@ -56,23 +105,6 @@ public class NuevoCERecyclerItemViewHolder extends RecyclerView.ViewHolder {
                     int id = menuItem.getItemId();
                     Trabajo t = Trabajo.load(Trabajo.class, Long.parseLong(partidaName.getTag().toString()));
                     switch (id) {
-                        case R.id.action_plus1:
-                            t.cantidad = t.cantidad + 1;
-                            t.partida.ranking = t.partida.ranking + 1;
-                            t.partida.save();
-                            t.save();
-                            partidaCont.setText(String.valueOf(t.cantidad));
-                            break;
-                        case R.id.action_plus10:
-                            t.cantidad = t.cantidad + 10;
-                            t.partida.ranking = t.partida.ranking + 1;
-                            t.partida.save();
-                            t.save();
-                            partidaCont.setText(String.valueOf(t.cantidad));
-                            break;
-                        case R.id.action_photo:
-                            Log.d("TAG", "Photo");
-                            break;
                         case R.id.action_obs:
                             AlertDialog.Builder builder = new AlertDialog.Builder(parent.getContext());
                             builder.setTitle("Observaciones");
@@ -97,15 +129,28 @@ public class NuevoCERecyclerItemViewHolder extends RecyclerView.ViewHolder {
                     return true;
                 }
             });
-            toolbar.setOnLongClickListener(new View.OnLongClickListener() {
-                @Override
-                public boolean onLongClick(View view) {
-                    Log.d("LongClick", String.valueOf(view.getId()));
-                    return false;
-                }
-            });
         }
-        return new NuevoCERecyclerItemViewHolder(parent, partidaName, partidaCont, trabajoObs, plus1, plus10, obs);
+
+        trabajoObs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String txt = trabajoObs.getText().toString();
+                if (txt != null && !txt.isEmpty()) {
+                    AlertDialog alertDialog = new AlertDialog.Builder(view.getContext()).create();
+                    alertDialog.setTitle(R.string.text_observaciones);
+                    alertDialog.setMessage(trabajoObs.getText());
+                    alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    dialog.dismiss();
+                                }
+                            });
+                    alertDialog.show();
+                }
+            }
+        });
+
+        return new NuevoCERecyclerItemViewHolder(parent, partidaName, partidaCont, trabajoObs, plus1, plus10);
     }
 
     public void setTrabajo(Trabajo t) {
